f594f3f9a5fdffd9364c3e1c328dcc1365c6805e
Muhammad Hussain <122493431+muhammadHussain18@users.noreply.github.com>
2023-02-04 21:20:03 +0500

    smit assignment 1

---
+++ smit assignment 14/app.js
@@ -0,0 +1,5 @@
+alert ("Error! Please enter a valid password") ;
+alert ("Welcome to JS land \n Happy coding!") ;
+alert ("Welcome to JS land...") ;
+alert (" Happy coding! \n Prevent this page from creating additional dialogs") ;
+alert ("Hello... I can run JS through my web browser's console") ;
